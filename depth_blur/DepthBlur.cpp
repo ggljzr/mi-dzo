bf410004e9ee4544e5e3abc332b041f678a26f42
Ondřej Červenka <cerveon3@fit.cvut.cz>
2017-03-17 17:01:18 +0100

    added depth bluring

--- depth_blur/DepthBlur.cpp
+++ depth_blur/DepthBlur.cpp
@@ -8,6 +8,31 @@
 
 using namespace std;
 
+void print_matrix(const cv::Mat * mat)
+{
+    printf("Size: %d r %d c\n", mat->rows, mat->cols);
+    printf("Type: %d, channels: %d\n", mat->type(), mat->channels());
+
+    int rows = mat->rows;
+    int channels = mat->channels();
+    int cols = mat->cols * channels;
+
+    for(int i = 0; i < rows; i++)
+    {
+        const uchar * row = mat->ptr<uchar>(i);
+        for(int j = 0; j < cols; j += channels )
+        {
+            printf("[ ");
+            for(int k = 0; k < channels; k++)
+            {
+                printf("c%d=%d, ", k, row[j + k]);
+            }
+            printf(" ], ");
+        }
+        printf("\n");
+    }
+}
+
 cv::Mat * get_neighbours(const cv::Mat * mat, int x, int y, int size)
 {
     cv::Rect r(y-size/2,x-size/2,size, size);
@@ -60,32 +85,7 @@ int get_average(const cv::Mat * mat, char color)
 	return sum;
 }
 
-void print_matrix(const cv::Mat * mat)
-{
-	printf("Size: %d r %d c\n", mat->rows, mat->cols);
-	printf("Type: %d, channels: %d\n", mat->type(), mat->channels());
-
-	int rows = mat->rows;
-	int channels = mat->channels();
-	int cols = mat->cols * channels;
-
-	for(int i = 0; i < rows; i++)
-	{
-		const uchar * row = mat->ptr<uchar>(i);
-		for(int j = 0; j < cols; j += channels )
-		{
-			printf("[ ");
-			for(int k = 0; k < channels; k++)
-			{
-				printf("c%d=%d, ", k, row[j + k]);
-			}
-			printf(" ], ");
-		}
-		printf("\n");
-	}
-}
-
-int depth_blur(const cv::Mat * mat, cv::Mat * res)
+int depth_blur(const cv::Mat * mat, const cv::Mat * depth, cv::Mat * res)
 {
     if(mat->rows != res->rows || mat->cols != res->cols)
         return NOT_MATCHING_SIZE;
@@ -102,10 +102,15 @@ int depth_blur(const cv::Mat * mat, cv::Mat * res)
     for(int i = 1; i < rows; i++)
     {
     	const uchar * row_in = mat->ptr<uchar>(i);
+        const uchar * row_depth = depth->ptr<uchar>(i);
     	uchar * row_out = res->ptr<uchar>(i);
     	for(int j = 3; j < cols; j += channels)
     	{
-    		cv::Mat * neigbours = get_neighbours(mat, i, j / channels, 7);
+            int k = row_depth[j];
+            if(k % 2 == 0)
+                k++;
+
+    		cv::Mat * neigbours = get_neighbours(mat, i, j / channels, k);
 
     		row_out[j] = get_average(neigbours, 0);
     		row_out[j + 1] = get_average(neigbours, 1);
@@ -121,15 +126,26 @@ int depth_blur(const cv::Mat * mat, cv::Mat * res)
 int main( int argc, char** argv )
 {
 
-	cv::Mat image = cv::imread("desk_3_1.png");
-	cv::Mat depth = cv::imread("desk_3_1_depth.png");
+    if(argc < 3)
+    {
+        cout << "Usage: ./DepthBlur image.png depth.png" << endl;
+        return 0;
+    }
+
+	cv::Mat image = cv::imread(argv[1]);
+	cv::Mat depth = cv::imread(argv[2]);
+
+    double min, max;
+    cv::minMaxLoc(depth, &min, &max);
+    cout << min << endl;
+    cout << max << endl;
 
     cv::Mat B = cv::Mat::zeros(image.rows, image.cols, CV_8UC3);
     
-    depth_blur(&image, &B);
+    depth_blur(&image, &depth, &B);
 
-    cv::imshow("img", image);
-    cv::imshow("B", B);
+    //cv::imshow("img", image);
+    cv::imwrite("res.png", B);
 
     cv::waitKey(0);                                          // Wait for a keystroke in the window
     return 0;
