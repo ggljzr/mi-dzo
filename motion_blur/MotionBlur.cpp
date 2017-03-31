51ab0efd458f89fe06f2badef6fcd8c9ebc53111
ggljzr <gogo.lejzr@gmail.com>
2017-03-31 11:54:06 +0200

    minor changes

--- motion_blur/MotionBlur.cpp
+++ motion_blur/MotionBlur.cpp
@@ -5,16 +5,20 @@
 
 using namespace std;
 
-int get_sequence(const cv::Mat * img, int n, cv::Mat ** seq)
+#define NOT_SAME_TYPE -1
+
+int get_sequence(const cv::Mat * img, int n, cv::Mat ** seq, double x, double y)
 {
 
     cv::Mat img_expanded;
+    int rows = img->rows;
+    int cols = img->cols;
 
-    cv::copyMakeBorder(*img, img_expanded, 200, 200, 200, 200, cv::BORDER_CONSTANT);
+    cv::copyMakeBorder(*img, img_expanded, 0, n*y, 0, n*x, cv::BORDER_CONSTANT);
 
     for(int i = 0; i < n; i++)
     {
-        double m[2][3] = {{1.0, 0.0, i * 8.0}, {0.0, 1.0, i * 4.0}};
+        double m[2][3] = {{1.0, 0.0, i * x}, {0.0, 1.0, i * y}};
         cv::Mat M = cv::Mat(2, 3, CV_64F, m);
         seq[i] = new cv::Mat(img_expanded.size(), img_expanded.type());
         cv::warpAffine(img_expanded, *seq[i], M, img_expanded.size());
@@ -26,10 +30,11 @@ int get_sequence(const cv::Mat * img, int n, cv::Mat ** seq)
 //dst is passed via pointer and allocated within this
 //function because I dont know the exact size of dst
 //matrix until I call get_sequence()
-int get_blured(const cv::Mat * img, int n, cv::Mat * &dst)
+int get_blured(const cv::Mat * img, int n, cv::Mat * &dst,
+                double x = 8.0, double y = 4.0)
 {
     cv::Mat ** seq = new cv::Mat*[n];
-    get_sequence(img, n, seq);
+    get_sequence(img, n, seq, x, y);
 
     dst = new cv::Mat(seq[0]->size(), seq[0]->type());
 
@@ -37,7 +42,7 @@ int get_blured(const cv::Mat * img, int n, cv::Mat * &dst)
 
     for(int i = 0; i < n; i++)
     {
-        cv::addWeighted(*dst, 0.5, *seq[i], 0.5, 0, *dst);
+        cv::addWeighted(*dst, 0.6, *seq[i], 0.4, 0, *dst);
     }
 
     for(int i = 0; i < n; i++)
@@ -50,20 +55,58 @@ int get_blured(const cv::Mat * img, int n, cv::Mat * &dst)
     return 0;
 }
 
-int compose(const cv::Mat * fg, const cv::Mat * bg, cv::Mat * dst)
+int compose(const cv::Mat * fg, cv::Mat * bg, int x, int y)
 {
+    if(fg->channels() != 3)
+        cv::cvtColor(*fg, *fg, CV_GRAY2RGB);
+    
+    if(bg->channels() != 3)
+        cv::cvtColor(*bg, *bg, CV_GRAY2RGB);
 
-}
+    int rows = fg->rows;
+    int cols = fg->cols * fg->channels();
+
+    for(int i = 0; i < rows; i++)
+    {
+        uchar * row_bg = bg->ptr<uchar>(i + y);
+        const uchar * row_fg = fg->ptr<uchar>(i); 
+        for(int j = 0; j < cols; j += 3)
+        {
+            int r = row_fg[j];
+            int g = row_fg[j + 1];
+            int b = row_fg[j + 2];
+
+            if(r > 10 && g > 10 && b > 10)
+            {
+
+            row_bg[j + x] = r;
+            row_bg[j + x + 1] = g;
+            row_bg[j + x + 2] = b;}
+        }
+    }
+}   
 
 int main(int argc, char ** argv)
 {
-    int n = 10;
+    int n = 20;
 
-    cv::Mat img = cv::imread("smallball.png");
+    if(argc < 2)
+    {
+        cout << "usage: ./MotionBlur image.png" << endl;
+    }
+
+    cv::Mat img = cv::imread(argv[1]);
+    //cv::Mat bg = cv::imread("bg.png");
+
+    //cout << img.type() << endl;
+
     cv::Mat * dst = NULL;
-    get_blured(&img, n, dst);
+    get_blured(&img, n, dst, 16, 8);
+    
+    //compose(dst, &bg, 0, 0);
 
     cv::imwrite("res.png", *dst);
+    cv::imwrite("res.png", img);
 
     delete dst;
 
--- motion_blur/motion_blur.py
+++ motion_blur/motion_blur.py
@@ -1,33 +1,34 @@
 import cv2
 import numpy as np
+import sys
 
-def get_sequence(img, n):
+def get_sequence(img, n, x = 8, y = 4):
     seq = []
 
-    img_expanded = cv2.copyMakeBorder(img, img.shape[0] * 2, img.shape[0] * 2, 
-        img.shape[1] * 2, img.shape[1] * 2, cv2.BORDER_CONSTANT)
+    img_expanded = cv2.copyMakeBorder(img, 0, n*y, 0, n*x, cv2.BORDER_CONSTANT)
     rows = img_expanded.shape[0]
     cols = img_expanded.shape[1]
 
     for i in range(0, n):
-        M = np.float32([[1,0,8*i],[0,1,4*i]])
+        M = np.float32([[1,0,x*i],[0,1,y*i]])
         dst = cv2.warpAffine(img_expanded,M,(cols,rows))
         seq.append(dst)
 
     return seq
 
-img = cv2.imread('smallball.png',0)
-img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+im_name = 'smallball.png'
 
-print(bg.shape)
-print(img.shape)
+if len(sys.argv) > 1:
+    im_name = sys.argv[1]
 
-seq = get_sequence(img, 8)
+img = cv2.imread(im_name)
+
+seq = get_sequence(img, 20)
 
 blur = np.copy(seq[0])
 
-#for i in seq:
-#    blur = cv2.addWeighted(blur, 0.5, i, 0.5, 0.2)
+for i in seq:
+    blur = cv2.addWeighted(blur, 0.5, i, 0.5, 0.2)
 
 cv2.imwrite('res.png', img)
 cv2.waitKey(0)
