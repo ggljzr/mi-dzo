609d4944fb99c55aa2a57d410855c2d31e473865
ggljzr <gogo.lejzr@gmail.com>
2017-04-28 03:04:54 +0200

    hybrid images gaussian (WIP)

--- hybrid_images/hybrid_image.py
+++ hybrid_images/hybrid_image.py
@@ -1,18 +1,42 @@
-import cv2
 import numpy as np
-from matplotlib import pyplot as plt
+from numpy.fft import fft2, ifft2, fftshift, ifftshift
+import math
+import cv2
 
-ein = cv2.imread('ein250.png', 0)
-mon = cv2.imread('mon250.png', 0)
+#src: https://jeremykun.com/2014/09/29/hybrid-images/
+def makeGaussianFilter(numRows, numCols, sigma):
+   centerI = int(numRows/2) + 1 if numRows % 2 == 1 else int(numRows/2)
+   centerJ = int(numCols/2) + 1 if numCols % 2 == 1 else int(numCols/2)
+ 
+   def gaussian(i,j):
+      coefficient = math.exp(-1.0 * ((i - centerI)**2 + (j - centerJ)**2) / (2 * sigma**2))
+      return coefficient
+ 
+   return np.array([[gaussian(i,j) for j in range(numCols)] for i in range(numRows)])
 
-high = cv2.Sobel(ein, -1, 1, 1, scale=1, delta=128)
+def get_spec(img):
+   spec = fft2(img)
+   spec = fftshift(spec)
+   return spec
 
-low = cv2.GaussianBlur(mon, (7,7), 10)
+if __name__ == "__main__":
+   mon = cv2.imread("mon250.png", 0)
+   ein = cv2.imread("ein250.png", 0)
 
-print(high.shape)
-print(low.shape)
+   n,m = mon.shape
 
-res = cv2.addWeighted(high, 0.7, low, 0.3, 0)
+   gaussian = makeGaussianFilter(n, m, 20)
 
-cv2.imshow("aa", res)
-cv2.waitKey()
+   ein_spec = get_spec(ein)
+   mon_spec = get_spec(mon)
+   mon_spec = mon_spec * gaussian
+
+   diff = 0
+
+   low = ifft2(mon_spec)
+   high = ifft2(ein_spec * (1 - gaussian))
+
+   res = low + high
+   
+   cv2.imshow('aaa', np.abs(res) / 255)
+   cv2.waitKey()
